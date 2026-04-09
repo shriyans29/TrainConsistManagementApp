dfb928bb3537439844250dac9c2924c539b52a2c
shriyans29 <shriyans2906@gmail.com>
2026-04-09 08:53:14 +0530

    UC15: Safe Cargo Assignment Using try-catch-finally

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,26 +1,58 @@
-public static void main(String[] args){
+// Custom Runtime Exception
+class CargoSafetyException extends RuntimeException {
+    public CargoSafetyException(String message) {
+        super(message);
+    }
+}
 
-    System.out.println("===================================");
-    System.out.println("UC2 - Add Passenger Bogies to Train");
-    System.out.println("===================================");
+// Goods Bogie Class
+class GoodsBogie {
+    String bogieId;
+    String shape;
+    String cargo;
 
-    List<String> passengerBogies = new ArrayList<>();
+    public GoodsBogie(String bogieId, String shape) {
+        this.bogieId = bogieId;
+        this.shape = shape;
+    }
 
-    passengerBogies.add("Sleeper");
-    passengerBogies.add("AC Chair");
-    passengerBogies.add("First Class");
+    // UC15: Safe Cargo Assignment using try-catch-finally
+    public void assignCargo(String cargoType) {
+        try {
 
-    System.out.println("\n\nafter adding bogies:");
-    System.out.println(("Passenger Bogies : " + passengerBogies));
+            if (shape.equalsIgnoreCase("Rectangular") &&
+                    cargoType.equalsIgnoreCase("Petroleum")) {
+                throw new CargoSafetyException(
+                        "Unsafe: Cannot assign Petroleum to Rectangular Bogie " + bogieId);
+            }
 
-    passengerBogies.remove("\nAC Chair");
-    System.out.println("after removing AC Chair:");
-    System.out.println(("Passenger Bogies : " + passengerBogies));
 
-    System.out.println("\nchecking if 'Sleeper' exists:");
-    System.out.println("Contains Sleeper? : " + passengerBogies.contains("Sleeper"));
+            this.cargo = cargoType;
+            System.out.println("Cargo assigned successfully to Bogie " + bogieId);
 
-    System.out.println("\nFinal Train Passesnger Consist:");
-    System.out.println( passengerBogies);
+        } catch (CargoSafetyException e) {
 
+            System.out.println("Error: " + e.getMessage());
+
+        } finally {
+            System.out.println("Cargo assignment attempt completed for Bogie " + bogieId);
+        }
+    }
+}
+
+public class TrainConsistManagementApp {
+
+    public static void main(String[] args) {
+
+        GoodsBogie b1 = new GoodsBogie("GB1", "Cylindrical");
+        b1.assignCargo("Petroleum");
+
+
+        GoodsBogie b2 = new GoodsBogie("GB2", "Rectangular");
+        b2.assignCargo("Petroleum");
+
+
+        GoodsBogie b3 = new GoodsBogie("GB3", "Rectangular");
+        b3.assignCargo("Coal");
+    }
 }
