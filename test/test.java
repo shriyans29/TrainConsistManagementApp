5e65dedafed38aa183d21cfccead708e39cdc8ef
shriyans29 <96971658+shriyans29@users.noreply.github.com>
2026-04-17 09:23:16 +0530

    Add BubbleSortTest class with sorting implementation
    Implemented a Bubble Sort algorithm to sort an array of capacities.

---
+++ test/test.java
@@ -0,0 +1,26 @@
+public class BubbleSortTest {
+    public static void main(String[] args) {
+        int[] capacities = {72, 56, 24, 70, 60};
+
+        System.out.println("Original capacities:");
+        for (int c : capacities) {
+            System.out.println(c);
+        }
+
+        // Bubble sort
+        for (int i = 0; i < capacities.length - 1; i++) {
+            for (int j = 0; j < capacities.length - 1; j++) {
+                if (capacities[j] > capacities[j + 1]) {
+                    int temp = capacities[j];
+                    capacities[j] = capacities[j + 1];
+                    capacities[j + 1] = temp;
+                }
+            }
+        }
+
+        System.out.println("Sorted capacities:");
+        for (int c : capacities) {
+            System.out.println(c);
+        }
+    }
+}
